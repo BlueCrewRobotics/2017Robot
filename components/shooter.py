32e3b200a00dfcbbcd9c77c08759801b418629fd
Caleb-Mulcahy <32500486+Caleb-Mulcahy@users.noreply.github.com>
2017-12-05 16:39:21 -0500

    added spaces to Shooter.py

--- components/shooter.py
+++ components/shooter.py
@@ -1,14 +1,18 @@
 #imrpoting wpilib
 import wpilib
+
 #a class for the shooter
 class Shooter:
     shoot_motor = wpilib.Talon
+
     #defining the shooter to shoot
     def shoot(self):    
         self.shoot_motor.set(-0.35)
+
     #defining the shooter to stop  
     def stop(self):  
         self.shoot_motor.set(0)
+
     #has to have it in order to run
     def execute(self):
         pass
