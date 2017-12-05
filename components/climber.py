78b187d69af5fecf6b09358fb275d85173f2fecc
ChristopherRVance <32501837+ChristopherRVance@users.noreply.github.com>
2017-12-05 16:35:11 -0500

    Comments
    Added comments to Climber.py

--- components/climber.py
+++ components/climber.py
@@ -1,13 +1,16 @@
 import wpilib
-
+#Commands to make the robot climb.
 class Climber:
     climb_motor = wpilib.Talon
-
+    
+    #Set robot to climb when motor is on.
     def climb(self):
         self.climb_motor.set(1)
-
+    
+    #Stops the robot from climbing when motor is off.
     def stop_climb(self):
         self.climb_motor.set(0)
-
+    
+    #Execute is needed, but won't work with a given value.
     def execute(self):
         pass
